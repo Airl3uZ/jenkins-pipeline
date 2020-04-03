69631f7cad135650916a20c8ad5353065a5948ca
Airl3uZ <siriwat.jiws@gmail.com>
2020-04-03 17:43:24 +0700

    send id

--- pipeline/php/phpPipeline.groovy
+++ pipeline/php/phpPipeline.groovy
@@ -18,6 +18,7 @@ pipeline {
                         branch: "BUILD_T2PCHECKOUTAPI_DEV",
                         appenv: "dev",
                         repo: "build_configs"
+                        credentialsId: "75aa10b1-d3c0-4675-818f-73b572b08684"
                     )
                 }
                 sh "mkdir api_checkoutv3"
@@ -26,6 +27,7 @@ pipeline {
                         branch: "DEVELOP",
                         appenv: "dev",
                         repo: "dev_api_t2pcheckoutv3"
+                        credentialsId: "75aa10b1-d3c0-4675-818f-73b572b08684"
                     )
                 }
                 sh "mkdir _inc"
@@ -34,6 +36,7 @@ pipeline {
                         branch: "_INC_MAIN",
                         appenv: "dev",
                         repo: "dev_inc_main"
+                        credentialsId: "75aa10b1-d3c0-4675-818f-73b572b08684"
                     )                 
                 }
             }
