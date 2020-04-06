8ecc88b67b78818ff740c6ce0160bfed43219c53
Airl3uZ <siriwat.jiws@gmail.com>
2020-04-06 19:08:48 +0700

    split out qualitygate steps from sonarqube

--- pipeline/Test-Pipeline/phpPipeline.groovy
+++ pipeline/Test-Pipeline/phpPipeline.groovy
@@ -43,10 +43,9 @@ pipeline {
                         echo "Do Static code analysis with SonarQube"
                         withSonarQubeEnv('T2P-SonarQube') {   
                             sh "${scannerHome}/bin/sonar-scanner -Dproject.settings=${env.WORKSPACE}/sonar-project.properties"
-                            // submitted SonarQube taskId is automatically attached to the pipeline context
-                            timeout(time: 10, unit: 'MINUTES') {
-                                waitForQualityGate abortPipeline: true
-                            }
+                        }
+                        timeout(time: 10, unit: 'MINUTES') {
+                            waitForQualityGate abortPipeline: true
                         }
                     }
                 }
