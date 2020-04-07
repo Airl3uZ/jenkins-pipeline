c879d0b0c7d9bfbd60dd0bf760ae1d125003c9c3
Airl3uZ <siriwat.jiws@gmail.com>
2020-04-07 11:19:15 +0700

    test

--- pipeline/php/phpPipeline.groovy
+++ pipeline/php/phpPipeline.groovy
@@ -5,19 +5,20 @@ pipeline {
     stages {
         stage("Checkout APP") {
             steps {
-                checkout([$class: 'GitSCM', branches: [[name: 'origin/DEVELOP']], doGenerateSubmoduleConfigurations: false,extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'data']],userRemoteConfigs: [[credentialsId: '75aa10b1-d3c0-4675-818f-73b572b08684', url: 'git@dev-www.ibaht.com:dev_cfg.git']]])
+                echo "step one"
+                // checkout([$class: 'GitSCM', branches: [[name: 'origin/DEVELOP']], doGenerateSubmoduleConfigurations: false,extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'data']],userRemoteConfigs: [[credentialsId: '75aa10b1-d3c0-4675-818f-73b572b08684', url: 'git@dev-www.ibaht.com:dev_cfg.git']]])
             }
         }
-        stage('checkout api_checkout') {
-            steps {
-                checkout([$class: 'GitSCM', branches: [[name: 'origin/DEVELOP']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'data/api_checkout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '75aa10b1-d3c0-4675-818f-73b572b08684', url: 'git@dev-www.ibaht.com:dev_api_t2pcheckoutv3.git']]])
-            }
-        }
-        stage('Checkout INC') {
-            steps {
-                checkout([$class: 'GitSCM', branches: [[name: 'origin/DEVELOP']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'data/_inc']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '75aa10b1-d3c0-4675-818f-73b572b08684', url: 'git@dev-www.ibaht.com:dev_inc.git']]]) 
-            }
-        }
+        // stage('checkout api_checkout') {
+        //     steps {
+        //         checkout([$class: 'GitSCM', branches: [[name: 'origin/DEVELOP']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'data/api_checkout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '75aa10b1-d3c0-4675-818f-73b572b08684', url: 'git@dev-www.ibaht.com:dev_api_t2pcheckoutv3.git']]])
+        //     }
+        // }
+        // stage('Checkout INC') {
+        //     steps {
+        //         checkout([$class: 'GitSCM', branches: [[name: 'origin/DEVELOP']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'data/_inc']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '75aa10b1-d3c0-4675-818f-73b572b08684', url: 'git@dev-www.ibaht.com:dev_inc.git']]]) 
+        //     }
+        // }
         stage("Tests") {
             parallel {
                 stage('UnitTest') {
@@ -41,20 +42,18 @@ pipeline {
                     }
                 }
                 stage('SonarQube code analysis and Quality Gate') {
-                    environment {
-                        scannerHome = tool name: 'sonar-scanner'
-                    }
+                    // environment {
+                    //     scannerHome = tool name: 'sonar-scanner'
+                    // }
                     steps {
                         // sh "printenv"
                         echo "Do Static code analysis with SonarQube"
-                        withSonarQubeEnv('T2P-SonarQube') { 
-                            // echo "${env.WORKSPACE}"
-                            // sh "pwd && ls -altr"  
-                            sh "${scannerHome}/bin/sonar-scanner -Dproject.settings=${env.WORKSPACE}/scripts/pipeline/Test-Pipeline/sonar-project.properties"
-                        }
-                        timeout(time: 10, unit: 'MINUTES') {
-                            waitForQualityGate abortPipeline: true
-                        }
+                        // withSonarQubeEnv('T2P-SonarQube') { 
+                        //     sh "${scannerHome}/bin/sonar-scanner -Dproject.settings=${env.WORKSPACE}/scripts/pipeline/Test-Pipeline/sonar-project.properties"
+                        // }
+                        // timeout(time: 10, unit: 'MINUTES') {
+                        //     waitForQualityGate abortPipeline: true
+                        // }
                     }
                 }
             }
