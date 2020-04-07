bea137e03930897139403e16e23bbcd821d77757
Airl3uZ <siriwat.jiws@gmail.com>
2020-04-07 22:28:54 +0700

    fix println qg

--- pipeline/Test-Pipeline/phpPipeline.groovy
+++ pipeline/Test-Pipeline/phpPipeline.groovy
@@ -52,7 +52,7 @@ pipeline {
                         timeout(time: 1, unit: 'HOURS') { 
                             script {
                                 def qg = waitForQualityGate()
-                                sh "println ${qg}"
+                                println("${qg}")
                                 if (qg.status != 'OK') {
                                     error "Pipeline aborted due to quality gate failure: ${qg.status}"
                                 }
