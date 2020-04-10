115f9437967273b770cacf46137daab458d24c0a
Airl3uZ <siriwat.jiws@gmail.com>
2020-04-10 21:59:13 +0700

    update jenkinsfile

--- pipeline/Test-Pipeline/phpPipeline.groovy
+++ pipeline/Test-Pipeline/phpPipeline.groovy
@@ -16,11 +16,11 @@ pipeline {
                 )
             }
         }
-        stage("Tests") {
+        stage('UnitTest') {
             agent {
                 docker {
                     args "-v ///${pwd}/data/app:/app:rw"
-                    image 'webdevops/php'
+                    image 'phpunit/phpunit:latest'
                     reuseNode true
                 }
             }
@@ -35,68 +35,39 @@ pipeline {
                     sh 'composer update'
                     sh 'ls'
                     echo "Unit Test"
-                    sh './vendor/bin/phpunit'
+                    sh 'phpunit'
                 }
             }
         }
-            // parallel {
-                stage('UnitTest') {
-                //     agent {
-                //         docker {
-                //             args "-v ///${pwd}/data/app:/app:rw"
-                //             image 'webdevops/php'
-                //             reuseNode true
-                //         }
-                //     }
-                //     options {
-                //         timeout(time: 10, unit: "MINUTES")
-                //     }
-                //     steps {
-                //         dir('data/app') {
-                //             echo "check environment"
-                //             sh "pwd ls -altr && whoami && hostname"
-                //             echo "Composer Update"
-                //             sh 'composer update'
-                //             sh 'ls'
-                //             echo "Unit Test"
-                            // sh './vendor/bin/phpunit'
-                //         }
-                //     }
-                    steps {
-                        echo "Test"
-                    }
+        stage('SonarQube code analysis and Quality Gate') {
+            environment {
+                scannerHome = tool name: 'sonar-scanner'
+            }
+            steps {
+                // sh "printenv"
+                echo "Do Static code analysis with SonarQube"
+                withSonarQubeEnv('T2P-SonarQube') { 
+                    // echo "${env.WORKSPACE}"
+                    // sh "pwd && ls -altr"  
+                    sh "${scannerHome}/bin/sonar-scanner -Dproject.settings=data/sonar-project.properties"
                 }
-                stage('SonarQube code analysis and Quality Gate') {
-                    environment {
-                        scannerHome = tool name: 'sonar-scanner'
-                    }
-                    steps {
-                        // sh "printenv"
-                        echo "Do Static code analysis with SonarQube"
-                        withSonarQubeEnv('T2P-SonarQube') { 
-                            // echo "${env.WORKSPACE}"
-                            // sh "pwd && ls -altr"  
-                            sh "${scannerHome}/bin/sonar-scanner -Dproject.settings=data/sonar-project.properties"
+                // timeout(time: 10, unit: 'MINUTES') {
+                //     waitForQualityGate abortPipeline: true
+                // }
+                timeout(time: 1, unit: 'HOURS') { 
+                    script {
+                        def qg = waitForQualityGate()
+                        println("${qg}")
+                        if (qg.status != 'OK') {
+                            error "Pipeline aborted due to quality gate failure: ${qg.status}"
                         }
-                        // timeout(time: 10, unit: 'MINUTES') {
-                        //     waitForQualityGate abortPipeline: true
-                        // }
-                        timeout(time: 1, unit: 'HOURS') { 
-                            script {
-                                def qg = waitForQualityGate()
-                                println("${qg}")
-                                if (qg.status != 'OK') {
-                                    error "Pipeline aborted due to quality gate failure: ${qg.status}"
-                                }
-                                else {
-                                    echo "${qg.status}"
-                                }
-                            }
+                        else {
+                            echo "${qg.status}"
                         }
                     }
                 }
-            // }
-        // }
+            }
+        }
         // stage("OWASP dependency check") {
         //     steps {
         //         sh "mkdir -p report/owasp_dependency_check"
